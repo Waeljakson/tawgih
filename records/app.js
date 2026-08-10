3450e8b8a0b7aa5d35978feb1f3234a06e2dc297
Waeljakson <waeljaksonstar@gmail.com>
2026-08-10 21:55:54 +0300

    Add files via upload

--- records/app.js
+++ records/app.js
@@ -5,7 +5,7 @@ const SUPABASE_PUBLISHABLE_KEY = "sb_publishable_3C7eKHRkzE2T-OLOpfue4g_i3u4R7Ay
 const WHATSAPP_NUMBER = "966582712620";
 const CURRENT_PACKAGE_CODE = "guidance_records";
 const SCHOOL_EDITION=true;
-const SCHOOL_SCHEMA_VERSION="1.0.40";
+const SCHOOL_SCHEMA_VERSION="1.0.41";
 const UNIFIED_PLATFORM_ROUTES = {
   results_analysis: {label:"تحليل النتائج", href:"../analysis/index.html"},
   guidance_records: {label:"السجلات الرقمية", href:"../records/index.html"},
